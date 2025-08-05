f376a7dadbb1601f8215813af909d8adcdefbc7b
Nguyễn Đức Tùng <110727407+ductung3008@users.noreply.github.com>
2025-08-05 23:41:27 +0700

    feat: add category page ui

---
+++ src/data/products.ts
@@ -0,0 +1,162 @@
+export const products = [
+  {
+    id: '1',
+    name: 'iPhone 16e 128GB',
+    newPrice: 15990000,
+    oldPrice: 16999000,
+    imageUrl:
+      'https://shopdunk.com/images/thumbs/0034910_iphone-16e-128gb_240.png',
+    promotionPercentage: 5,
+    tag: 'new',
+  },
+  {
+    id: '2',
+    name: 'iPhone 16 Pro 128GB',
+    newPrice: 24690000,
+    oldPrice: 28990000,
+    imageUrl:
+      'https://shopdunk.com/images/thumbs/0030444_iphone-16-pro-128gb_240.png',
+    promotionPercentage: 14,
+    tag: 'installment',
+  },
+  {
+    id: '3',
+    name: 'iPhone 16 Pro Max 256GB',
+    newPrice: 29690000,
+    oldPrice: 34990000,
+    imageUrl:
+      'https://shopdunk.com/images/thumbs/0029155_iphone-16-pro-max-256gb_240.png',
+    promotionPercentage: 15,
+    tag: 'installment',
+  },
+  {
+    id: '4',
+    name: 'iPhone 16 128GB',
+    newPrice: 18790000,
+    oldPrice: 22990000,
+    imageUrl:
+      'https://shopdunk.com/images/thumbs/0030771_iphone-16-128gb_240.png',
+    promotionPercentage: 18,
+    tag: 'installment',
+  },
+  {
+    id: '5',
+    name: 'iPad Air (M3) 11-inch Wi-Fi',
+    newPrice: 16290000,
+    oldPrice: 16999000,
+    imageUrl:
+      'https://shopdunk.com/images/thumbs/0035054_ipad-air-m3-11-inch-wi-fi_240.png',
+    promotionPercentage: 4,
+    tag: 'new',
+  },
+  {
+    id: '6',
+    name: 'iPad (A16) 11 inch Wi-Fi',
+    newPrice: 9299000,
+    oldPrice: 9999000,
+    imageUrl:
+      'https://shopdunk.com/images/thumbs/0035754_ipad-a16-11-inch-wi-fi_240.png',
+    promotionPercentage: 7,
+    tag: 'new',
+  },
+  {
+    id: '7',
+    name: 'iPad Air 6 M2 13 inch Wi-Fi',
+    newPrice: 16190000,
+    oldPrice: 21990000,
+    imageUrl:
+      'https://shopdunk.com/images/thumbs/0025630_ipad-air-6-m2-13-inch-wi-fi_240.jpeg',
+    promotionPercentage: 26,
+    tag: 'installment',
+  },
+  {
+    id: '8',
+    name: 'iPad Gen 10 th 10.9 inch WiFi 64GB',
+    newPrice: 8390000,
+    oldPrice: 12990000,
+    imageUrl:
+      'https://shopdunk.com/images/thumbs/0009725_ipad-gen-10-th-109-inch-wifi-64gb_240.png',
+    promotionPercentage: 35,
+    tag: 'installment',
+  },
+  {
+    id: '9',
+    name: 'MacBook Air M4 13 inch (8 core GPU | 16GB RAM | 256GB SSD)',
+    newPrice: 25690000,
+    oldPrice: 26999000,
+    imageUrl:
+      'https://shopdunk.com/images/thumbs/0036014_macbook-air-m4-13-inch-8-core-gpu-16gb-ram-256gb-ssd_240.jpeg',
+    promotionPercentage: 5,
+    tag: 'new',
+  },
+  {
+    id: '10',
+    name: 'MacBook Air M2 13 inch (10 core GPU | 16GB RAM | 256GB SSD)',
+    newPrice: 24090000,
+    oldPrice: 39990000,
+    imageUrl:
+      'https://shopdunk.com/images/thumbs/0034122_macbook-air-m3-13-inch-8gb-ram-256gb-ssd_240.jpeg',
+    promotionPercentage: 40,
+    tag: 'installment',
+  },
+  {
+    id: '11',
+    name: 'MacBook Air M1 2020 (8GB RAM | 256GB SSD)',
+    newPrice: 16890000,
+    oldPrice: 28990000,
+    imageUrl:
+      'https://shopdunk.com/images/thumbs/0034123_macbook-air-m2-13-inch-10-core-gpu-16gb-ram-256gb-ssd_240.jpeg',
+    promotionPercentage: 42,
+    tag: 'installment',
+  },
+  {
+    id: '12',
+    name: 'MacBook Pro M4 14 inch (10 core GPU | 16GB RAM | 512GB SSD)',
+    newPrice: 42990000,
+    oldPrice: 0,
+    imageUrl:
+      'https://shopdunk.com/images/thumbs/0034121_macbook-air-m1-2020-8gb-ram-256gb-ssd_240.jpeg',
+    promotionPercentage: 0,
+    tag: 'new',
+  },
+  {
+    id: '13',
+    name: 'Apple Watch Series 10 Nhôm (GPS) 42mm | Sport Band',
+    newPrice: 10490000,
+    oldPrice: 10990000,
+    imageUrl:
+      'https://shopdunk.com/images/thumbs/0029160_apple-watch-series-10-nhom-gps-42mm-sport-band_240.jpeg',
+    promotionPercentage: 5,
+    tag: 'new',
+  },
+  {
+    id: '14',
+    name: 'Apple Watch Ultra 2 GPS + Cellular 49mm Ocean (2024)',
+    newPrice: 22990000,
+    oldPrice: 0,
+    imageUrl:
+      'https://shopdunk.com/images/thumbs/0030238_apple-watch-ultra-2-gps-cellular-49mm-ocean-2024_240.png',
+    promotionPercentage: 0,
+    tag: 'new',
+  },
+  {
+    id: '15',
+    name: 'Apple Watch SE GPS + Cellular 2024 Sport Band size S/M',
+    newPrice: 6590000,
+    oldPrice: 7299000,
+    imageUrl:
+      'https://shopdunk.com/images/thumbs/0032869_apple-watch-se-gps-cellular-2024-sport-band-size-sm_240.jpeg',
+    promotionPercentage: 10,
+    tag: 'installment',
+  },
+  {
+    id: '16',
+    name: 'Apple Watch SE GPS 2024 Sport Band size S/M',
+    newPrice: 5690000,
+    oldPrice: 5999000,
+    imageUrl:
+      'https://shopdunk.com/images/thumbs/0030450_apple-watch-se-gps-2024-sport-band-size-sm_240.jpeg',
+    promotionPercentage: 5,
+    tag: 'installment',
+  },
+];

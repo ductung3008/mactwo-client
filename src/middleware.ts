441881f107cef54cfbb1d185479bb70faa22622e
ductung3008 <ductungltt@gmail.com>
2025-08-23 23:23:55 +0700

    chore: change admin-site domain

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,19 @@
 import createMiddleware from 'next-intl/middleware';
+import { NextRequest, NextResponse } from 'next/server';
 import { routing } from './i18n/routing';
 
-export default createMiddleware(routing);
+const intlMiddleware = createMiddleware(routing);
+
+export default function middleware(req: NextRequest) {
+  const host = req.headers.get('host') || '';
+
+  if (host === 'admin.mactwo.click') {
+    return NextResponse.next();
+  }
+
+  return intlMiddleware(req);
+}
 
 export const config = {
-  matcher: '/((?!api|trpc|_next|_vercel|.*\\..*|admin).*)',
+  matcher: '/((?!api|trpc|_next|_vercel|.*\\..*).*)',
 };
